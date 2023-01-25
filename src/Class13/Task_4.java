f5254816accaabb98d67cd7b0f770e01aa8cbb26
Rafik2018 <ismayilovrafik1@gmail.com>
2023-01-25 18:35:18 -0500

    Class13 Tasks

--- src/Class13/Task_4.java
+++ src/Class13/Task_4.java
@@ -15,24 +15,6 @@ public class Task_4 {
         System.out.println(str.reverse());
 
 
-        System.out.println("--------------------------------------------");
-
-
-        String s = "level";
-        String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev = rev + s.charAt(i);
-        }
-        if (rev.equals(s)) {
-            System.out.println("This is palindrome :" + s);
-        } else {
-            System.out.println("This is not palindrome");
-
-        }
-
-
-
-
     }
 
 
