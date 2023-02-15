a2f04ab873e89661fa916b75090201f126373329
Rafik2018 <ismayilovrafik1@gmail.com>
2023-02-15 18:48:19 -0500

    Class25

---
+++ src/Class25/WebDriverTester.java
@@ -0,0 +1,27 @@
+package Class25;
+
+public class WebDriverTester {
+
+
+    public static void main(String[] args) {
+
+
+    WebDriver [] webDrivers = {new WebDriver.ChromeDriver(),new WebDriver.FirefoxDriver()};
+
+    for (WebDriver w : webDrivers){
+
+        w.openBrowser();
+        w.closeBrowser();
+        w.maximizeWindow();
+        w.findElement();
+    }
+
+
+
+
+
+    }
+
+
+
+}
