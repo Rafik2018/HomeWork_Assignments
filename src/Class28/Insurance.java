d6574f41c3c023d7c7c65cc7f2f20f31940a5ab9
Rafik2018 <ismayilovrafik1@gmail.com>
2023-02-20 12:25:52 -0500

    Class28

--- src/Class28/Insurance.java
+++ src/Class28/Insurance.java
@@ -1,149 +1,149 @@
 package Class28;
 
+/*port java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+
+*//*
+6) Create a class Insurance that will have an attribute as insuranceName and unimplemented behaviour as getQuote and cancelInsurance.
+        Create 3 subclasses Car, Pet, Health. Car class has it’s own attribute as carModel and Class Pet has petType attribute.
+        Create 3 objects of the sub classes and store them in ArrayList. Using for loop/advanced for loop/ iterator access all methods of the class.
+        Create a Card class that will have interest rate field and card type
+        and a constructor that will initialize the fields.
+        Create 3 objects of different card and store them into LinkedList.
+        Using for loop/advanced for loop/ iterator access all methods of the class
+
+*/
+
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 
-public abstract class Insurance {
+abstract class Insurance {
+    protected String insuranceName;
 
-
-    public String insuranceName;
-
-    public Insurance(String insuranceName) {
-        this.insuranceName = insuranceName;
-    }
-
-     abstract double getQuote();
-     abstract void cancelInsurance();
-
+    public abstract void getQuote();
+    public abstract void cancelInsurance();
 }
 
 class Car extends Insurance {
-
-    public String carModel;
+    private String carModel;
 
     public Car(String insuranceName, String carModel) {
-        super(insuranceName);
+        this.insuranceName = insuranceName;
         this.carModel = carModel;
     }
-    @Override
-    public double getQuote() {
-        return 1000.0;
+
+    public void getQuote() {
+        System.out.println("AutoInsurance Quote is "+ 1000.00);
     }
 
-    public String CarModel(){
-        return carModel;
-    }
-    @Override
     public void cancelInsurance() {
-
+        System.out.println("Car Model is"+carModel);
     }
 }
+
 
 
 class Pet extends Insurance {
-
-    public String petType;
+    private String petType;
 
     public Pet(String insuranceName, String petType) {
-        super(insuranceName);
+        this.insuranceName = insuranceName;
         this.petType = petType;
     }
 
-    public String petType() {
-        return petType;
+    public void getQuote() {
+        System.out.println("PetInsurance Quote is "+ 500.00);
     }
 
-    public double getQuote() {
-        return 500.0;
-    }
-
-    @Override
     public void cancelInsurance() {
-
+        System.out.println("Pet Type is"+ petType);
     }
 }
-
 
 class Health extends Insurance {
-
     public Health(String insuranceName) {
-        super(insuranceName);
+        this.insuranceName = insuranceName;
     }
 
-    @Override
-    double getQuote() {
-        return 0;
+    public void getQuote() {
+        System.out.println(0.00);
     }
 
-    @Override
     public void cancelInsurance() {
+        System.out.println("Health Insurance cancelled ");
+
+        System.out.println("---------------------------------");
     }
 
 
-    public static class Card {
 
-        public String cardType;
-        public double interestRate;
 
-        public Card(String cardType, double interestRate) {
-            this.cardType = cardType;
-            this.interestRate = interestRate;
-        }
 
-        public String getCardType() {
-            return cardType;
 
-        }
+    public static void main(String[] args) {
+        ArrayList<Insurance> insuranceList = new ArrayList<>();
+        insuranceList.add(new Car("Car Insurance", " Honda Civic"));
+        insuranceList.add(new Pet("Pet Insurance", " Dog"));
+        insuranceList.add(new Health("Health Insurance"));
 
-        public double getInterestRate() {
-            return interestRate;
+        for (Insurance insurance : insuranceList) {
+            insurance.getQuote();
+            insurance.cancelInsurance();
 
         }
 
+        LinkedList<Card> cardList = new LinkedList<>();
+        cardList.add(new Card("Visa", 12.5));
+        cardList.add(new Card("MasterCard", 11.75));
+        cardList.add(new Card("Amex", 15.0));
 
-        public static void main(String[] args) {
-
-
-            ArrayList<Insurance> insuranceList = new ArrayList<>();
-
-
-            Car carInsurance = new Car("Car Insurance AAA", "Toyota");
-            Pet petInsurance = new Pet("Pet Insurance PPP", "Dog");
-            Health healthInsurance = new Health("Health Insurance HHH ");
-            insuranceList.add(carInsurance);
-            insuranceList.add(petInsurance);
-            insuranceList.add(healthInsurance);
-            for (Insurance currentInsurance : insuranceList) {
-                System.out.println(currentInsurance.insuranceName);
-                System.out.println(currentInsurance.getQuote());
-                currentInsurance.cancelInsurance();
-
-
-
-            }
-
-            System.out.println("---------------------------------------------");
-
-
-            LinkedList<Card> cardList = new LinkedList<>();
-
-            Card card1 = new Card("Visa", 22.00);
-            Card card2 = new Card("MasterCard", 23.99);
-            Card card3 = new Card("Amex", 24.99);
-            cardList.add(card1);
-            cardList.add(card2);
-            cardList.add(card3);
-
-            for (Card card : cardList) {
-                System.out.println(card.cardType);
-                System.out.println(card.interestRate);
-            }
-
+        for (Card card : cardList) {
+            System.out.println("Card type: " + card.getCardType());
+            System.out.println("Interest rate: " + card.getInterestRate());
         }
-
     }
 }
+
+class Card {
+    private String cardType;
+    private double interestRate;
+
+    public Card(String cardType, double interestRate) {
+        this.cardType = cardType;
+        this.interestRate = interestRate;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
