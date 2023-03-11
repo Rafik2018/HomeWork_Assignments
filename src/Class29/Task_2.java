ce97e903225da69de70d1e3e7697c0ca7932604c
Rafik2018 <ismayilovrafik1@gmail.com>
2023-03-10 23:10:04 -0500

    HW30R

--- src/Class29/Task_2.java
+++ src/Class29/Task_2.java
@@ -1,33 +1,31 @@
 package Class29;
 
-import java.util.HashMap;
-import java.util.HashMap;
-import java.util.Objects;
-
+import java.util.LinkedHashSet;
 
 public class Task_2 {
+
+
     public static void main(String[] args) {
-        /*
-        Create a Set collection that will hold Objects of Student Type.
-         In this set we do not care about the insertion order.
-         Each student object should have name and studentID.
-          Display name of each student.
-         */
 
-        HashMap<Integer, String> students=new HashMap<>();
-        students.put(1," Adam");
-        students.put(2," Brain");
-        students.put(3," Clare");
-        students.put(4," Daniel");
-        students.put(5," Ethan");
-        students.put(6," Frank");
+   /*
+      Create a Set of cities in which you want to make sure that insertion order is maintained.
+          Then remove any city that starts with “A”;
+       */
 
-        System.out.println(students.keySet());
+            LinkedHashSet<String> cities=new LinkedHashSet<>();
+            cities.add("Richmond");
+            cities.add("FairFax");
+            cities.add("Mclean");
+            cities.add("Annandale");
 
-        for(Object st :students.keySet()){
-            System.out.print(students.get(st));
+            cities.removeIf(x->x.startsWith("A")||x.startsWith("a"));
+            System.out.println(cities);
         }
     }
-}
+
+
+
+
+
 
 
